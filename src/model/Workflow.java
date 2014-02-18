fae2eafc8370dfe8354caf725942633caee174ac
joellundell <joel.lundell@gmail.com>
2014-02-18 10:56:49 +0100

    Added java project to generate classes too

---
+++ src/model/Workflow.java
@@ -0,0 +1,11 @@
+public class Workflow {
+
+	public List<Task> tasks = new ArrayList<>();
+	
+	public myWorkFlow {
+    Task Task1 = new Task();
+	tasks.add(n);
+    Task Task2 = new Task();
+	tasks.add(n);
+	}		
+}

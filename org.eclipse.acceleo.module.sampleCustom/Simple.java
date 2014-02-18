f6c2bc84e874a341b40237980397d302cfd147eb
andergu <anders@ajgustafsson.se>
2014-02-18 14:43:01 +0100

    Updated things

---
+++ org.eclipse.acceleo.module.sampleCustom/Simple.java
@@ -0,0 +1,16 @@
+public class Simple extends Transition {
+
+	private String simepl2;
+	private Task org.eclipse.emf.ecore.impl.DynamicEObjectImpl@27ea6b67 (eClass: org.eclipse.emf.ecore.impl.EClassImpl@3fbe59d5 (name: Task) (instanceClassName: null) (abstract: false, interface: false));
+	private Task org.eclipse.emf.ecore.impl.DynamicEObjectImpl@391eb14a (eClass: org.eclipse.emf.ecore.impl.EClassImpl@3fbe59d5 (name: Task) (instanceClassName: null) (abstract: false, interface: false));
+
+	public Simple(String name, Task task, Task prevTask) {
+		this.name = name;
+		this.task = task;
+		this.prevTask = prevTask;
+	}
+	
+	
+
+
+}
